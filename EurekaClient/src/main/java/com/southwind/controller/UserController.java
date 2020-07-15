46b91fd3b2cf984439a79ef2e2bfe02301025969
grjcl <guyuepan@163.com>
2020-07-15 22:15:20 +0800

    spring-cloud-初始

---
+++ EurekaClient/src/main/java/com/southwind/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.southwind.controller;
+
+import com.southwind.entity.Student;
+import com.southwind.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/student")
+public class UserController {
+    @Autowired
+    private UserService studentRepository;
+
+    @GetMapping("/findAll")
+    public Collection<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/findById/{id}")
+    public Student findById(@PathVariable("id") long id) {
+        return studentRepository.findById(id);
+
+    }
+    @PostMapping("/save")
+    public void save (@RequestBody Student student){
+        studentRepository.saveOrUpdate(student);
+    }
+    @PutMapping("/update")
+    public void update (@RequestBody Student student){
+        studentRepository.saveOrUpdate(student);
+    }
+    @DeleteMapping("/deleteById/{id}")
+    public void deleteById ( @PathVariable("id") long id){
+        studentRepository.deleteById(id);
+    }
+}
+
