d9722f354637e41818377a79f30d7e0ffa385301
grjcl <58839410+grjcl@users.noreply.github.com>
2020-07-26 16:00:55 +0800

    Update RestHandler.java
    增加密码

--- Consumer/src/main/java/com/southwind/controller/RestHandler.java
+++ Consumer/src/main/java/com/southwind/controller/RestHandler.java
@@ -10,7 +10,7 @@ import java.util.Collection;
 @RestController
 @RequestMapping("/con")
 public class RestHandler {
-
+public Integer password;
     @Autowired
     private RestTemplate restTemplate;
     @GetMapping("/findAll")
